eab3efac5ca9f776db531ae0b8431b3432d6fdc7
Gia <PXC_Gia@users.noreply.github.com>
2020-09-08 19:53:49 -0400

    env additions to token

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const {App} = require("@slack/bolt");
 
 //Auth Bot
+const bot = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.BOT_TOKEN
+})
 
 /*(async()=>{
     await bot.start(process.env.PORT || 3000);
